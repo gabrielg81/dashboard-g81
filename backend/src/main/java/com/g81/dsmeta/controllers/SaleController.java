58716e57441ffc1a14fa06e81632fbe6cd811d62
Gabriel Silva da Cruz <gabrielscruz.g81@gmail.com>
2022-07-18 02:24:46 -0300

    twilio sms

--- backend/src/main/java/com/g81/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/g81/dsmeta/controllers/SaleController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.g81.dsmeta.entities.Sale;
 import com.g81.dsmeta.services.SaleService;
+
+import com.g81.dsmeta.services.SmsService;
+
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,11 +23,20 @@ public class SaleController {
 @Autowired
 	private SaleService service;
 
+@Autowired
+private SmsService smsService;
+
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(value="minDate", defaultValue = "") String minDate, 
 			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
