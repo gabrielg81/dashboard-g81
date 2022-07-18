717b30a49c8a50b3338ae8747073b327b1e24eef
Gabriel Silva da Cruz <gabrielscruz.g81@gmail.com>
2022-07-18 01:58:14 -0300

    date select

--- backend/src/main/java/com/g81/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/g81/dsmeta/controllers/SaleController.java
@@ -1,5 +1,8 @@
 package com.g81.dsmeta.controllers;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,10 @@ public class SaleController {
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 }
