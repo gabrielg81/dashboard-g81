ad2b296c7a4acece5aaeb18ccdf54d78028dda28
Gabriel Silva da Cruz <gabrielscruz.g81@gmail.com>
2022-07-18 00:28:50 -0300

    first component

---
+++ frontend/src/components/Login/index.tsx
@@ -0,0 +1,12 @@
+import NotificationButton from "../NotificationButton";
+
+function Login() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <NotificationButton />
+    </>
+  );
+}
+
+export default Login;
